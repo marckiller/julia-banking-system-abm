a2e434343b0dbcdd6a877e1975b1142eb9bc5175
Marcel Półkośnik <marcel.polkosnik@student.uw.edu.pl>
2025-07-22 00:00:14 +0200

    sheduled event basic architecture

--- src/simulation.jl
+++ src/simulation.jl
@@ -7,3 +7,19 @@ struct Simulation
     #banks::Vector{Agent}
 end
 
+function schedule_event!(sim::Simulation, event::Event)
+    enqueue!(sim.event_queue, event, event.time)
+end
+
+function run!(sim::Simulation, end_time::Int)
+    while !isempty(sim.event_queue) && sim.current_time < end_time
+        event = dequeue!(sim.event_queue)
+        sim.current_time = event.time
+        # Process the event
+    end
+end
+
+function handle_event!(sim::Simulation, event::Event)
+    println("[$(event.time)] Event: $(event.type), payload: $(event.payload)")
+    # TODO handle the event based on its type
+end
