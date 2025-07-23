161b73db36ee05f6027b0334195ac17ffb2b729c
Marcel Półkośnik <marcel.polkosnik@student.uw.edu.pl>
2025-07-24 01:42:35 +0200

    missing loan_id

--- src/loan.jl
+++ src/loan.jl
@@ -1,6 +1,7 @@
 abstract type Loan end
 
 struct BulletLoan <: Loan
+    id::Int
     principal::Int
     annual_interest_rate::Float64
     term::Int #in days
@@ -11,8 +12,8 @@ struct BulletLoan <: Loan
     lender::Symbol
 end
 
-function BulletLoan(principal::Int, annual_interest_rate::Float64, term::Int, issue_date::Int, borrower::Symbol, lender::Symbol)
+function BulletLoan(id::Int,principal::Int, annual_interest_rate::Float64, term::Int, issue_date::Int, borrower::Symbol, lender::Symbol)
     end_time = issue_date + term
     repay_amount = round(Int, principal * (1 + annual_interest_rate / 365) ^ term)
-    return BulletLoan(principal, annual_interest_rate, term, issue_date, end_time, repay_amount, borrower, lender)
+    return BulletLoan(id, principal, annual_interest_rate, term, issue_date, end_time, repay_amount, borrower, lender)
 end
