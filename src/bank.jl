96003f91dc42035d18e14626676b3736101e6497
Marcel Półkośnik <marcel.polkosnik@student.uw.edu.pl>
2025-07-24 01:37:34 +0200

    remove bank/consumer loan distinction

--- src/bank.jl
+++ src/bank.jl
@@ -3,10 +3,8 @@ mutable struct Bank
     reserves::Int
     min_reserves_frac::Float64
 
-    total_deposit_liability::Int
-    total_loan_assets::Int
-    total_interbank_assets::Int
-    total_interbank_liabilities::Int
+    total_liabilities::Int
+    total_assets::Int
 
     credit_interest_rate::Float64
     deposit_interest_rate::Float64
@@ -16,7 +14,7 @@ end
 function Bank(id::Int, reserves::Int, min_reserves_frac::Float64,
                credit_interest_rate::Float64, deposit_interest_rate::Float64,
                interbank_credit_rate::Float64)
-    return Bank(id, reserves, min_reserves_frac,
-                0, 0, 0, 0,
-                credit_interest_rate, deposit_interest_rate, interbank_credit_rate)
+    return Bank(id, reserves, min_reserves_frac, 0, 0, 
+    credit_interest_rate, deposit_interest_rate, 
+    interbank_credit_rate)
 end
