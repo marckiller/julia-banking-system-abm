7b39c26ec344f7c13cdfa23b32c063cc63f044a3
Marcel Półkośnik <marcel.polkosnik@student.uw.edu.pl>
2025-07-23 23:21:57 +0200

    new SimulationState structure

--- src/simulation.jl
+++ src/simulation.jl
@@ -11,12 +11,12 @@ mutable struct SimulationState
     loans::Dict{Int, Loan}
     events::PriorityQueue{Int, Event}
 
-    # Additional fields for simulation state
+    #Additional fields for simulation state
     total_deposits::Int
     total_loans::Int
     total_interbank_assets::Int
     total_interbank_liabilities::Int
 
-    # Statistics for the simulation
+    #Statistics for the simulation
     statistics::Dict{Symbol, Any}
 end
